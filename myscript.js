6c3e709212af8af1332ed927375b84bd905e3dbd
Matteo Lopez <matteolopezbb@gmail.com>
2022-02-23 19:55:33 +0100

    refactoring

--- myscript.js
+++ myscript.js
@@ -1,5 +1,5 @@
 
-const tripDistance = document.getElementById('trip-distance');
+const tripDistance = document.getElementById('trip-distance').value;
 console.log("tripDistance");
 
 let userAgeInput = document.getElementById("user-age");
@@ -8,28 +8,29 @@ let generatedEvent = document.getElementById('generated-event');
 
 let userNameInput = document.getElementById("full-name");
 
-// let tripDurationInput = document.getElementById("trip-distance");
 
 generatedEvent.addEventListener("click", function(){
     document.querySelector('h1').innerHTML = userNameInput.value;
+
     document.getElementById('final-price').innerHTML = priceBase.value;
+
     console.log(priceBase);
-    // document.querySelector('h2').innerHTML = tripDurationInput.value;
+    
     if (userAgeInput.value = "Minorenne"){
+
         teenPrice = priceBase - discountTeen;
-    } else if (userAgeInput){
-        priceBase = priceBase;
-    };
+
+        document.getElementById("final-price").innerHTML = teenPrice;
+    ;}
+    
     
 });
 
-let priceBase = parseInt (0.27 * tripDistance);
+
+let priceBase = parseFloat (0.27 * tripDistance);
 console.log(priceBase);
 
 
 const discountTeen = parseInt ((priceBase * 17) / 100);
 console.log(discountTeen);
-
-// const discountOld = ((priceBase * 40) / 100);
-// console.log(discountOld);
 
