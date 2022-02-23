60a7f05645daf6ddda45cf3902c2428c5fbcb65f
Matteo Lopez <matteolopezbb@gmail.com>
2022-02-23 18:36:15 +0100

    refactoring e correzioni + inserimento user name  nella sezione biglietto

--- myscript.js
+++ myscript.js
@@ -3,33 +3,39 @@ const tripDistance = document.getElementById('trip-distance');
 console.log("tripDistance");
 
 
+
 let userAge = "";
 
-let generatedEvent = document.getElement('generated-event')
+let generatedEvent = document.getElementById('generated-event');
 
-userAgeUnder = document.getElementById('user-ages').value = 'Minorenne';
-console.log('userAgesUnder');
+// userAgeUnder = document.getElementById('user-ages').value = 'Minorenne';
+// console.log('userAgesUnder');
 
-userAgeOver = document.getElementById('user-ages').value = 'Maggiorenne';
-console.log('userAgesOver');
+// userAgeOver = document.getElementById('user-ages').value = 'Maggiorenne';
+// console.log('userAgesOver');
+
+let userNameInput = document.getElementById("full-name");
 
 
-generatedEvent.addEventListener("click", function{
-    if (userAgeUnder){
+
+generatedEvent.addEventListener("click", function(){
+    document.querySelector('h1').innerHTML = userNameInput.value;
+    
+    if (userAge){
         teenPrice = priceBase - discountTeen;
-    } else if (userAgeOver){
+    } else if (userAge){
         priceBase = priceBase;
     };
     
 });
 
-let priceBase = parseFloat(0.27 * tripDistance);
-console.log(priceBase);
+// let priceBase = (0.27 * tripDistance);
+// console.log(priceBase);
 
 
-const discountTeen = parseFloat((priceBase * 17) / 100);
-console.log(discountTeen);
+// const discountTeen = parseFloat((priceBase * 17) / 100);
+// console.log(discountTeen);
 
-const discountOld = parseFloat((priceBase * 40) / 100);
-console.log(discountOld);
+// const discountOld = parseFloat((priceBase * 40) / 100);
+// console.log(discountOld);
 
